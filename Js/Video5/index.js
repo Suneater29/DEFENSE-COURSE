9670b3dacf3e49a689f52b404d32639c74772427
Suneater29 <arsalan8974@gmail.com>
2026-07-12 21:03:22 +0530

    July 12 '26

--- Js/Video5/index.js
+++ Js/Video5/index.js
@@ -1,3 +1,7 @@
-let a =100;
-let b=23.87848367;
-console.log(b.toFixed(3));
+// let a =100;
+// let b=23.87848367;
+// console.log(typeof b.toFixed(3));
+let a=new Number(40);
+let b=new Number(40);
+console.log(a===b);
+// console.log(typeof a);
