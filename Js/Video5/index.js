0c6b03fe3263566755df29d27e8eb5fb78db4155
Suneater29 <arsalan8974@gmail.com>
2026-07-15 16:59:28 +0530

    July 15 '26

--- Js/Video5/index.js
+++ Js/Video5/index.js
@@ -1,7 +1,13 @@
 // let a =100;
 // let b=23.87848367;
 // console.log(typeof b.toFixed(3));
-let a=new Number(40);
-let b=new Number(40);
-console.log(a===b);
+// let a=new Number(40);
+// let b=new Number(40);
+// console.log(a===b);
 // console.log(typeof a);
+
+// let a = Math.random()*10;
+// console.log(Math.floor(Math.random()*10)*6);
+
+//OTP generator
+console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
