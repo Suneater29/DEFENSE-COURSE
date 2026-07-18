ab28e4bf5c7b10a097b536bb9f5d86a8e394be3e
Suneater29 <arsalan8974@gmail.com>
2026-07-19 00:23:16 +0530

    July 19 '26

---
+++ Js/Video6/index.js
@@ -0,0 +1,34 @@
+// console.log("arsalan");
+// console.log(`arsalan will be 
+// a pro coder`);
+
+
+// const date=23;
+// const  str=`my birthday is on March ${date}`;
+
+
+// console.log(str);
+// console.log(str.length);
+// console.log(str[4]);
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+
+// console.log(str.indexOf("day"));
+// console.log(str.indexOf("2"));
+
+
+// console.log(str.slice(-6,-1));
+
+
+// console.log(str.substring(5,7));
+
+// let a = "arsal";
+// let b = " ansari";
+// console.log(a+b);
+
+const current=new Date;
+console.log(current.toString());
+console.log(current.getDay());
---
+++ Js/Video7/index.js
@@ -0,0 +1,4 @@
+let arr=[100,90,"arsal"];
+let arr1=[100,90,"arsal",true];
+console.log(arr);
+console.log(arr1);
