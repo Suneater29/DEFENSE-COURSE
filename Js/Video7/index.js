4749a467e9e68b17d5db87797f911db6fe5df805
Suneater29 <arsalan8974@gmail.com>
2026-07-20 21:09:14 +0530

    July 20 '26

--- Js/Video7/index.js
+++ Js/Video7/index.js
@@ -1,4 +1,35 @@
-let arr=[100,90,"arsal"];
-let arr1=[100,90,"arsal",true];
-console.log(arr);
-console.log(arr1);
+// let arr=[100,90,30,88];
+// // let arr1=[100,90,"arsal",true];
+
+
+// // console.log(arr);
+// // console.log(arr1);
+
+
+// // arr.unshift(30);
+
+// // arr.push(209);
+// // arr.pop();
+
+// let arr2=arr;
+// arr2.push(33);
+
+// console.log(arr2);
+
+// const arr=[10,20,30,40,50];
+
+
+// const arr2=arr.slice(2,4);
+// console.log(arr);
+// console.log(arr2);
+// const arr3=arr.splice(0,3,22,33,44,99);
+// console.log(arr3); 
+
+// const arr=[101,2,330,407,950];
+// // arr.sort((a,b)=>a-b);
+// arr.sort((a,b)=>b-a);
+// // arr.sort();
+// console.log(arr);
+
+const arr=[2,7,8,[90,66,54,6],45,33];
+console.log(arr.flat());
